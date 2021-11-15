e042033a6f23be975faeb8111a8ba8a9d03726f8
Isaac Ikusika <helloayodeji@gmail.com>
2021-11-15 06:39:29 +0100

    more setup: transfered dai and dapp as necessary

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,5 +1,28 @@
 const TokenFarm = artifacts.require("TokenFarm");
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+
+  //accounts is all the addresses from ganache, and we are using it here cos we need to give some dai to an account just for test
+  //
+
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  await deployer.deploy(TokenFarm, daiToken.address, dappToken.address);
+  const tokenFarm = await TokenFarm.deployed(); //we assigned the deployed to a variable just so we could use the address from the variable
+
+  //transfer 1 million dapp token to token farm on default so we can distribute to investors later
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  await daiToken.transfer(accounts[1], '100000000000000000000'); //give token to one of the address on ganache - the second guy i.e. the investor
+
 };
+
+//since the Token Farm contract has a constructr that takes two parameter (dapp and dai), 
+//we need to find a way to pass these addresses to the contract, i.e. make it aware of the addresses.
+//the way to do this is to import the two addresses here, then pass them to the token farm as it is being deployed to the blockchain
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.7.0 < 0.9.0;
 
+import "./DaiToken.sol";
+import "./DappToken.sol";
+
 contract TokenFarm {
     string public name = "TokenFarm";
+    DaiToken private daiToken;
+    DappToken private dappToken;
+
+    constructor(DaiToken _daiToken, DappToken _dappToken) {
+        daiToken = _daiToken;
+        dappToken = _dappToken;
+    }
 }

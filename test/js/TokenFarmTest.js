61dd7fd35f043a2301b970e5192ac118e81604e8
Isaac Ikusika <helloayodeji@gmail.com>
2021-11-17 10:47:06 +0100

    added more tests

--- test/js/TokenFarmTest.js
+++ test/js/TokenFarmTest.js
@@ -1,27 +1,29 @@
-const { assert } = require('chai');
+// const { assert } = require('chai');
 
-const TokenFarm = artifacts.require('TokenFarm')
-const DaiToken = artifacts.require('DaiToken')
-const DappToken = artifacts.require('DappToken')
+// const TokenFarm = artifacts.require('TokenFarm')
+// const DaiToken = artifacts.require('DaiToken')
+// const DappToken = artifacts.require('DappToken')
 
-require('chai')
-.use(require('chai-as-promised'))
-.should();
+// require('chai')
+// .use(require('chai-as-promised'))
+// .should();
 
-contract('TokenFarm', (accounts) => {
-    let tokenFarm;
+// contract('TokenFarm', (accounts) => {
+//     let tokenFarm, daiToken, dappToken;
     
-    before(async () => {
-        tokenFarm = await TokenFarm.deployed();
-    });
+//     before(async () => {
+//         dappToken = await DappToken.new();
+//         daiToken = await DaiToken.new();
+//         tokenFarm = await TokenFarm.new(daiToken.address, dappToken.address);
+//     });
 
-    describe("contract name", async () => {
-        it("has the right name", async () => {
-            //let tokenFarm = await TokenFarm.new();
-            let actualName = tokenFarm.name;
-            let expectedName = "TokenFarm";
-            assert(expectedName == actualName, "because that is the right name");
-        })
+//     describe("contract name", async () => {
+//         it("has the right name", async () => {
+//             //let tokenFarm = await TokenFarm.new();
+//             let actualName = tokenFarm.name;
+//             let expectedName = "TokenFarm";
+//             assert(expectedName == actualName, "because that is the right name");
+//         })
         
-    })
-})
+//     })
+// })
--- test/sol/TokenFarmTest.sol
+++ test/sol/TokenFarmTest.sol
@@ -4,18 +4,48 @@ pragma solidity >= 0.7.0 < 0.9.0;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../../src/contracts/TokenFarm.sol";
+import "../../src/contracts/DappToken.sol";
+import "../../src/contracts/DaiToken.sol";
 
 contract TokenFarmTest {
 
+    TokenFarm tokenFarm;
+    DappToken dappToken;
+    DaiToken daiToken;
+    uint ethAmount;
+
+    function beforeEach() public {
+    
+    address thisAddress = address(this); 
+    ethAmount = 1000000000000000000000000;
+
+    daiToken = new DaiToken();
+    dappToken = new DappToken();
+
+    tokenFarm = new TokenFarm(daiToken, dappToken);
+    dappToken.transfer(address(tokenFarm), ethAmount);
+    daiToken.transfer(thisAddress, ethAmount);
+
+  }
+
+    // function toWei(uint num) public returns (uint){
+    //     return num * 1 wei;
+    // }
 
     function test_contract_has_the_right_name() public {
-        
-        TokenFarm tokenFarm = TokenFarm(DeployedAddresses.TokenFarm());
-        
-        //string expectedName = "TokenFarm";
         
         string memory actualName = tokenFarm.getName();
         
         Assert.equal(actualName, "TokenFarm", "because the contract name shold be correct");
+    }
+
+    function test_that_tokenFarm_Has_1million_dapp_after_migration() public{
+        uint balance = dappToken.balanceOf(address(tokenFarm));
+        Assert.equal(balance, ethAmount, "because 1m eth was sent to it");
+    }
+
+    function test_that_sender_Has_1million_dai_after_migration() public{
+        uint thisAddressBalance = dappToken.balanceOf(address(this));
+        Assert.equal(thisAddressBalance, ethAmount, "because 1m eth was sent to it");
     }
 }

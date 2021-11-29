d54f7206be5056aa42d7bdf7c6ee9cb95a6a4ac2
Isaac Ikusika <helloayodeji@gmail.com>
2021-11-29 07:13:48 +0100

    import contracts to frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import DaiToken from '../abis/DaiToken.json'
 import './App.css'
 import Web3 from 'web3'
 
@@ -13,7 +14,26 @@ class App extends Component {
 async loadBlockchainData(){
   const web3 = window.web3;
   let accounts = await web3.eth.getAccounts();
+  console.log(accounts);
   this.setState({account: accounts[0]})
+
+  const networkId = await web3.eth.net.getId();
+  console.log(networkId);
+
+  const daiTokenData = DaiToken.networks[networkId];
+  if(daiTokenData){
+    const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+    this.setState({daiToken});
+    console.log(daiTokenData.address);
+    let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call();
+    console.log("here");
+    this.setState({daiTokenBalance: daiTokenBalance.toString()})
+    console.log({daiTokenBalance});
+  }
+  else{
+    alert("notdeployed contract")
+  }
+
 }
 async loadWeb3(){
   if(window.ethereum){
@@ -31,7 +51,14 @@ async loadWeb3(){
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0',
+      daiToken: {},
+      dappToken: {},
+      tokenFarm: {},
+      daiTokenBalance: "0",
+      dappTokenBalance: "0",
+      stakingBalance: "0",
+      loading: true
     }
   }
 
